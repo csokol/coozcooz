a32275da1cf8662bdc121d53903f56a65d5d73a1
Alberto Bueno Junior <alberto.bueno.jr@gmail.com>
2012-05-26 18:03:20 -0300

    Estrutura basica e gitignore

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link href="css/base.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+	<script type="text/javascript" src="js/something.js"></script>
+	
+	<title>Cooz Cooz</title>
+</head>
+<body>
+	<a href="#" class="something">Something</a>
+</body>
+</html>
