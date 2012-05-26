fa94c132d31fde3a8204294286a56f5ae4f4e5fc
Alberto Bueno Junior <alberto.bueno.jr@gmail.com>
2012-05-26 19:14:15 -0300

    Estilos...

--- index.php
+++ index.php
@@ -2,13 +2,30 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<link href="css/base.css" rel="stylesheet" type="text/css" />
+	<link href='http://fonts.googleapis.com/css?family=Oleo+Script|Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
+	<link href="css/styles.css" rel="stylesheet" type="text/css" />
 	<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
 	<script type="text/javascript" src="js/something.js"></script>
 	
 	<title>Cooz Cooz</title>
 </head>
 <body>
-	<a href="#" class="something">Something</a>
+	<div id="menu">
+		<div id="menuWrapper" class="roundedBordersBottom">
+			<div id="logo">
+				Cooz Cooz
+			</div>
+			<div id="controls">
+				<ul>
+					<li><a href="#">Criar conta</a></li>
+					<li><a href="#">Entrar</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div id="content" class="roundedBorders">
+		<h1>Quero cozinhar</h1>
+		<a href="#" class="something">Something</a>
+	</div>
 </body>
 </html>
