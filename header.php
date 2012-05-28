a8c623f1adfc9a9d381593377334836a98afe616
Alberto Bueno Junior <alberto.bueno.jr@gmail.com>
2012-05-28 17:18:37 -0300

    Busca refinada

--- header.php
+++ header.php
@@ -3,6 +3,20 @@
 		<div id="logo">
 			<a href="index.php">Cooz Cooz</a>
 		</div>
+		<?php 
+			if (isset($_GET['c']) && $_GET['c'] == "search") {
+				?>
+				<div id="refineSearch">
+					<form id="refineSearchForm">
+						<div id="search" class="roundedBorders" >
+							<input type="text" id="searchInput" />
+							<button type="submit"></button>
+						</div>
+					</form>
+				</div>
+				<?php
+			}
+		?>
 		<div id="controls">
 			<ul>
 				<li><a href="#">Criar conta</a></li>
