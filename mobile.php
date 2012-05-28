04103e4bb20912abcf7df39a7911699ae09f7924
André Casimiro <ah.casimiro@gmail.com>
2012-05-28 16:27:54 -0300

    Adiciona interface mobile

---
+++ mobile.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link href='http://fonts.googleapis.com/css?family=Oleo+Script|Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
+	<link href="css/common.css" rel="stylesheet" type="text/css" />
+	<link href="css/mobile.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+	<script type="text/javascript" src="js/search.js"></script>
+	<script type="text/javascript" src="js/tag.js"></script>
+	<script type="text/javascript" src="js/functions.js"></script>
+	
+	<title><?php echo "Strogonoff de Frango" ?></title>
+</head>
+<body>
+	<h1 id="logo">Cooz Cooz</h1>
+	<div id="content" class="roundedBorders">
+		<h2 id="recipeName">Strogonoff de Frango</h2>
+		<img id="photo" alt="foto" src="images/recipes/chicken.jpg"/>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet dapibus ipsum. Duis sit amet congue augue. Nulla sodales metus et metus viverra vitae fermentum risus sollicitudin. Aliquam a turpis felis, id aliquet lorem. Maecenas euismod, diam ut fermentum vehicula, purus tortor sodales quam, nec scelerisque purus augue non purus. Aenean mattis vestibulum ipsum ut bibendum. Curabitur a lorem orci, in fermentum enim. Aenean justo purus, congue sed faucibus ac, pretium eu ligula. Duis sagittis semper augue, sit amet faucibus justo condimentum eget. Vestibulum laoreet magna aliquam libero dictum a ultricies lectus molestie. Integer lectus lectus, tincidunt non commodo at, mollis quis lectus.
+		</p>
+	</div>
+</body>
+</html>
