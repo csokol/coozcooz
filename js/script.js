927a0134a753f69ac8be5ceb6d2a139ebd5cd659
Francisco Sokol <chico.sokol@gmail.com>
2012-05-28 00:30:56 -0300

    colocando as classes certas e dando um tapinha no js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,14 @@ $(document).ready(function() {
     $("#search-bar").hide();
     
     $("input.shouldCleanOnFocus").focus(function() {
+        if (!$(this).attr("defaultValue"))
+            $(this).attr("defaultValue", $(this).val());
         $(this).val("");
+    });
+    
+    $("input.shouldCleanOnFocus").blur(function() {
+        if ($(this).val().length == 0)
+            $(this).val($(this).attr("defaultValue"));
     });
     
     $("#home-search").submit(function() {
