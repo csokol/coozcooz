5e8e816346a98bb12d0bd0116bc0314a0bcf6742
André Casimiro <ah.casimiro@gmail.com>
2012-05-29 19:29:30 -0300

    Melhor página de visualização de receitas

--- recipe.php
+++ recipe.php
@@ -7,18 +7,19 @@
         include("goodies.php");    
     ?>
 	<div id="content" class="roundedBorders">
+    <div id="main">
     <?php
         include("db.php");
         if (isset($_GET['id'])) {
             $id = $_GET['id'];
         }
         else {
-            echo "<h4>Parâmetros inválidos!</h4>";
+            echo "<h1>Parâmetros inválidos!</h1>";
             die();
         }
         $db = new DB();
         if ($db->getRecipe($id) == null) {
-            echo "<h4>A receita solicitada não existe!</h4>";
+            echo "<h1>A receita solicitada não existe!</h1>";
             die();
         }
         
@@ -32,52 +33,66 @@
         echo "<br /><p>Visualizar no celular:</p>
               <img src='http://api.qrserver.com/v1/create-qr-code/?data={$img_src}' />";
         echo "</div>";
-        
-        $thumbs_html = "<ul class='ei-slider-thumbs'>
-                        <li class='ei-slider-element'>Current</li>";
 
-        echo "<div id='ei-slider' class='ei-slider'>
-                  <ul class='ei-slider-large'>";
-        foreach ($recipe['photos'] as $photo) {
-		    echo "<li>
-                      <img src='images/recipes/$photo' />
-                  </li>";
-            $thumbs_html .= "<li><a href='#'>Slide</a><img src='images/recipes/{$photo}' /></li>\n";
-        }
-        echo "</ul>";
-        $thumbs_html .= "</ul>";
-        echo $thumbs_html;
-        echo "</div>";
+        $photo = $recipe['photos'][0];
+        echo "<img style='float: left;' src='images/recipes/$photo'/>";
+        
+      //   $thumbs_html = "<ul class='ei-slider-thumbs'>
+      //                   <li class='ei-slider-element'>Current</li>";
+
+      //   echo "<div id='ei-slider' class='ei-slider'>
+      //             <ul class='ei-slider-large'>";
+      //   foreach ($recipe['photos'] as $photo) {
+		    // echo "<li>
+      //                 <img src='images/recipes/$photo' />
+      //             </li>";
+      //       $thumbs_html .= "<li><a href='#'>Slide</a><img src='images/recipes/{$photo}' /></li>\n";
+      //   }
+      //   echo "</ul>";
+      //   $thumbs_html .= "</ul>";
+      //   echo $thumbs_html;
+      //   echo "</div>";
 
     ?>        
 
-<?php include_once("ingredientsBar.php"); ?>
 <?php
         /* Ingredients list and directions */        
         echo "<div id='recipeDetail'>
-              <h4>Ingredientes</h4>
+              <h1>Ingredientes</h1>
                 <ul>";
         foreach ($recipe['ingredients'] as $key => $ingredient_id) {
-            echo "<li class='ingredient'>{$db->ingredients[$ingredient_id]}</li> ({$recipe['quantities'][$key]})";
+            echo "<li>{$recipe['quantities'][$key]} <span class='ingredient'>{$db->ingredients[$ingredient_id]}</span></li>";
         }
         echo "</ul>";
-        echo "<h4>Modo de Preparo</h4>
+        echo "<h1>Modo de Preparo</h1>
               <p>{$recipe['directions']}</p>";
 
         /* Show evaluations */
         echo "<br />";
-        echo "<h4>Avaliações</h4>";
+        echo "<h1>Avaliações</h1>";
         foreach ($recipe['evaluations'] as $evaluation) {
             echo "<br />
                   <div style='width: 180px'>
-                  <a href='#' class='notImplemented'><img src='images/users/{$evaluation['photo']}' width='60' /></a>
-                  <span style='float: right'><b> {$evaluation['user']}</b>
-                  ". htmlGrade($evaluation['rate']) ."<span></div>
-                    <p>  {$evaluation['comments']}</p>";
+                  <a href='#' class='notImplemented'>
+                    <img style='float: left; margin: 5px;' src='images/users/{$evaluation['photo']}' width='60' />
+                  </a>
+                  <span>
+                  <b>{$evaluation['user']}</b>
+                  <div style='display: inline-block'>".htmlGrade($evaluation['rate'])."</div>
+                  <span></div>
+                  <p style='float: left'>{$evaluation['comments']}</p>
+                  <div style='clear: both'></div>
+                  ";
         }
         echo "</div>";
         
     ?>
-	</div>
+    </div>
+
+    <?php include_once("ingredientsBar.php"); ?>
+
+    </div>
+
+
 </body>
 </html>
